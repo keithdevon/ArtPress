8371d99f110f301af06eba256a86e89791cab3b9
kdev <keithdevon@gmail.com>
2011-04-06 10:49:22 +0100

    Reduced code weight for color options. Started array, need to program for...each loop.

--- artpress-style-css.php
+++ artpress-style-css.php
@@ -2,71 +2,75 @@
 
 <?php
 
-    // TODO investigate alternatives to wp-load.php
-    
-	require_once('../../../wp-load.php');
+// TODO investigate alternatives to wp-load.php
 
-    $options = get_option('artpress_theme_options');
-    
-    // LINK COLOR
-    echo 'a:link, a:visited {color:'
-    	.$options['radioinput'].
-    	';}';
-    	
-    // FONT SIZE
-    echo '#content {font-size:'
-    	.$options['sometext'].
-    	';}';
-    	
-    // BODY BACKGROUND
-      echo 'body {background:'
-    	.$options['backgroundcolor'].
-    	';}';
-    	
-     // SITE TITLE
-      if($options['logo-color'] == 'primary' ) {
-      	echo '#site-title a {color:'
-    		.$options['primarycolor'].
-    		';}';
-    	}
-    	elseif($options['logo-color'] == 'secondary' ) {
-      	echo '#site-title a {color:'
-    		.$options['secondarycolor'].
-    		';}';
-    	}
-    	elseif($options['logo-color'] == 'tertiary' ) {
-      	echo '#site-title a {color:'
-    		.$options['tertiarycolor'].
-    		';}';
-    	}
-    	elseif($options['logo-color'] == 'background' ) {
-      	echo '#site-title a {color:'
-    		.$options['backgroundcolor'].
-    		';}';
-    	}
-    	
-	// PAGE TITLE
-	 if($options['title-color'] == 'primary' ) {
-      	echo '#site-title a {color:'
-    		.$options['primarycolor'].
-    		';}';
-    	}
-    	elseif($options['title-color'] == 'secondary' ) {
-      	echo '#site-title a {color:'
-    		.$options['secondarycolor'].
-    		';}';
-    	}
-    	elseif($options['title-color'] == 'tertiary' ) {
-      	echo '#site-title a {color:'
-    		.$options['tertiarycolor'].
-    		';}';
-    	}
-    	elseif($options['title-color'] == 'background' ) {
-      	echo '#site-title a {color:'
-    		.$options['backgroundcolor'].
-    		';}';
-    	}
+require_once('../../../wp-load.php');
+
+$options = get_option('artpress_theme_options');
+
+// LINK COLOR
+echo 'a:link, a:visited {color:'
+	.$options['radioinput'].
+	';}';
 	
+// FONT SIZE
+echo '#content {font-size:'
+	.$options['sometext'].
+	';}';
+	
+// BODY BACKGROUND
+  echo 'body {background:'
+	.$options['backgroundcolor'].
+	';}';
+	
+// SITE COLORS
+ 
+$site_elements = array(
+	'logo' => "#site-title a",
+	'title' => ".entry-title a:link, .entry-title:visited"
+	);
+ 
+css_color("#site-title a", $options['logo-color']);
+	
+	function css_color( $css_el, $color ) {
+    	global $options;
+    
+		echo $css_el . '{color:' ;
+		switch ($color) {
+	        case "primary": echo $options['primarycolor'];
+				break;
+			case "secondary": echo $options['secondarycolor'];
+				break;
+			case "tertiary": echo $options['tertiarycolor'];
+				break;
+			case "background": echo $options['backgroundcolor'];
+				break;
+		}
+		echo ';}';
+    }
+    
+	
+// PAGE TITLE
+
+?>	
+	.entry-title a:link, .entry-title a:visited {color:
+	<?php
+	
+	switch ($options['title-color'])
+	{
+		case "primary": echo $options['primarycolor'];
+			break;
+		case "secondary": echo $options['secondarycolor'];
+			break;
+		case "tertiary": echo $options['tertiarycolor'];
+			break;
+		case "background": echo $options['backgroundcolor'];
+			break;
+	}?>
+	
+	;}
+	<?php
+
 	
 	
 
