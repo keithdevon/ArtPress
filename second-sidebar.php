aeb3b629d1a222e6517b32be151fc61f3600e4ea
Keith <keithdevon@gmail.com>
2011-04-29 08:38:37 +0100

    Add files

---
+++ second-sidebar.php
@@ -0,0 +1,11 @@
+<?php
+	// A second sidebar for widgets, just because.
+	if ( is_active_sidebar( 'secondary-widget-area' ) ) : ?>
+
+		<div id="secondary" class="widget-area" role="complementary">
+			<ul class="xoxo">
+				<?php dynamic_sidebar( 'secondary-widget-area' ); ?>
+			</ul>
+		</div><!-- #secondary .widget-area -->
+
+<?php endif; ?>
---
+++ three-column.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Three columns, sidebar, content, sidebar
+ *
+ * A custom page template without sidebar.
+ *
+ * The "Template Name:" bit above allows this to be selectable
+ * from a dropdown menu on the edit page screen.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+
+		<div id="container" class="one-column row">
+		  <div class="threecol"><?php get_sidebar();?></div>
+			<div id="content" class="sixcol" role="main">
+
+			<?php
+			/* Run the loop to output the page.
+			 * If you want to overload this in a child theme then include a file
+			 * called loop-page.php and that will be used instead.
+			 */
+			 get_template_part( 'loop', 'page' );
+			?>
+
+			</div><!-- #content -->
+			
+			<div class="threecol last"><?php get_template_part( 'second-sidebar' );?></div>
+			
+		</div><!-- #container -->
+
+<?php get_footer(); ?>
