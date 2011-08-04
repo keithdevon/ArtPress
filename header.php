553a7462c5cf650819cc1e67351ef27c05b3921c
Keith <keithdevon@gmail.com>
2011-08-04 23:08:06 +0100

    Moved the generated css under wp_head

--- header.php
+++ header.php
@@ -40,6 +40,20 @@
 
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
+<?php get_template_part( 'cssgrid' );           // CSS Grid files (cssgrid.php) 
+	/* We add some JavaScript to pages with the comment form
+	 * to support sites with threaded comments (when in use).
+	 */
+	if ( is_singular() && get_option( 'thread_comments' ) )
+		wp_enqueue_script( 'comment-reply' );
+
+	/* Always have wp_head() just before the closing </head>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to add elements to <head> such
+	 * as styles, scripts, and meta tags.
+	 */
+	wp_head();
+?>
 <style type='text/css' media='screen'>
 <?php 
 // TODO http://core.trac.wordpress.org/ticket/14673
@@ -57,20 +71,6 @@ if($options = get_option('ap_options')) {
 }
 ?>
 </style>
-<?php get_template_part( 'cssgrid' );           // CSS Grid files (cssgrid.php) 
-	/* We add some JavaScript to pages with the comment form
-	 * to support sites with threaded comments (when in use).
-	 */
-	if ( is_singular() && get_option( 'thread_comments' ) )
-		wp_enqueue_script( 'comment-reply' );
-
-	/* Always have wp_head() just before the closing </head>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to add elements to <head> such
-	 * as styles, scripts, and meta tags.
-	 */
-	wp_head();
-?>
 </head>
 
 <body <?php body_class(); ?>>
