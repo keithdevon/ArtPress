10b04fb61ea01cf17e85f0d95696e9d8eb73b0b5
Keith Devon <keithdevon@gmail.com>
2011-12-01 17:40:00 +0000

    Create archives for Collections and Galleries

---
+++ archive-ap_collections.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying Gallery Archive pages.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+
+		<div id="content" class="container" role="main">
+            <div class="row">
+                <div class="twelvecol last">
+                <?php // TODO: make this conditional get_template_part( 'ht-crumbs' );?>
+				<h1 class="page-title">Collections</h1>
+				</div>
+            <div class="clear"></div>
+				<?php
+					$category_description = category_description();
+					if ( ! empty( $category_description ) )
+						echo '<div class="archive-meta row">' . $category_description . '</div>';
+
+				/* Run the loop for the category page to output the posts.
+				 * If you want to overload this in a child theme then include a file
+				 * called loop-category.php and that will be used instead.
+				 */
+				get_template_part( 'loop', 'galleries' );
+				?>
+
+
+		</div><!-- #container -->
+
+
+<?php get_footer(); ?>
