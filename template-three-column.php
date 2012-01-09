33ed4e4051fc37073b1c4293b5fbc2868926869e
Keith Devon <keithdevon@gmail.com>
2012-01-09 12:53:24 +0000

    Rename page templates

---
+++ template-three-column.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Three columns, sidebar, content, sidebar
+ *
+ * A custom page template without sidebar.
+ *
+ * The "Template Name:" bit above allows this to be selectable
+ * from a dropdown menu on the edit page screen.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+    <div id="content" class="container">
+		<div class="three-columns row">
+		  <div class="threecol"><?php get_template_part('sidebar');?></div>
+			<div id="content" class="sixcol" role="main">
+
+			<?php get_template_part( 'ht-crumbs' );?>
+
+			<?php
+			/* Run the loop to output the page.
+			 * If you want to overload this in a child theme then include a file
+			 * called loop-page.php and that will be used instead.
+			 */
+			 get_template_part( 'loop', 'page' );
+			?>
+
+			</div><!-- #content -->
+
+			<div class="threecol last"><?php get_template_part( 'sidebars/second-sidebar' );?></div>
+
+			<div class="clear"></div>
+
+		</div><!--  -->
+    </div><!-- #content -->
+<?php get_footer(); ?>
