ce07b0d121db04a724c097423bf67f020901b9ab
Keith Devon <keithdevon@gmail.com>
2012-01-09 13:00:22 +0000

    Remove excess meta from page templates

--- template-blank-right.php
+++ template-blank-right.php
@@ -4,12 +4,6 @@
  *
  * A custom page template without sidebar.
  *
- * The "Template Name:" bit above allows this to be selectable
- * from a dropdown menu on the edit page screen.
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
  */
 
 get_header(); ?>
